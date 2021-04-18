e8eedc6806a02d0834dbad96c823d912a8ce4097
wswytd <295212923@qq.com>
2021-04-18 23:15:45 +0800

    modify

--- fileinfo.py
+++ fileinfo.py
@@ -19,3 +19,18 @@ import time
 
 if sys.version_info >= (3, 0):
     raw_input = input
+
+file_name = raw_input("Enter a file name: ")  # pick a file you have
+count = 0
+t_char = 0
+try:
+    with open(file_name) as f:
+        line = f.readline()
+        t_char += len(line)
+        while line:
+            count += 1
+            line = f.readline()
+            t_char += len(line)
+except FileNotFoundError as e:
+    print(e)
+    sys.exit()
